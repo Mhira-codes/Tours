54ef99d19e03846483a1e35b2d441d3679a065c2
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-16 18:06:24 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useState } from 'react';
+import './App.css';
+import Loading from './component/Loading';
+const URL ='https://course-api.com/react-tours-project';
 function App() {
+
+const [loading, setLoading] = useState(true);
+
+
+const fetchData = async() => {
+  const response = await fetch(URL);
+  const tourData = await response.json();
+  console.log(tourData)
+};
+
+
+useEffect(()=>{
+  fetchData()
+},[])
+
+if(loading){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-orange-200 w-full h-screen font-sans">
+<Loading/>
     </div>
   );
+}
+
 }
 
 export default App;
