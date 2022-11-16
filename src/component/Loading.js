f621e5e3a9769d655fabe1e88350d9f87056bbff
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-16 18:08:16 +0000

    loading.js file

---
+++ src/component/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+        <h1 className='text-center font-medium text-5xl text-amber-900 pt-10'>Loading...</h1>
+    
+        </div>
+  )
+}
+
+export default Loading
