03881452b6220bdaf54228f960c539a60ab1b08e
mhira-codes <alhassansamiratu991@gmail.com>
2022-12-06 19:33:29 +0000

    changes in file

--- src/component/Tours.js
+++ src/component/Tours.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Tour from "./Tour";
 
-const Tours = () => {
+const Tours = ({ tourData }) => {
+  // console.log(tourData)
   return (
-    <div>Tours</div>
-  )
-}
+    <section className="bg-purple-50 w-full min-h-screen">
+      <div className="pt-10">
+        <h1 className="text-4xl text-center text-slate-900 font-semibold  underline underline-offset-8 decoration-slate-900">
+          Our Tours
+        </h1>
+      </div>
+      <div>
+        {tourData.map((tourItem) => {
+        return <Tour key={tourItem.id} {...tourItem}></Tour>
+     
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Tours
+export default Tours;
