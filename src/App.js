639e5dc34648424541fd322ea18839f90f269d40
mhira-codes <alhassansamiratu991@gmail.com>
2022-12-06 19:32:39 +0000

    changes in file

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Loading from './component/Loading';
+import Tours from './component/Tours';
 const URL ='https://course-api.com/react-tours-project';
 function App() {
 
 const [loading, setLoading] = useState(true);
+const [tourData,   setTourData] = useState([]);
+
+const removeTour =(id) =>{
+  const newTour = tourData.filter((tourItem)=> tourItem.id !== id)
+}
 
 
-const fetchData = async() => {
-  const response = await fetch(URL);
-  const tourData = await response.json();
-  console.log(tourData)
-};
 
+const fetchData = async()=>{
+  setLoading(true)
+
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json() ;
+    setLoading(false)
+    setTourData(data)
+
+  } catch (error) {
+    setLoading(false)
+    console.log(error)
+  }
+
+
+}
+ 
 
 useEffect(()=>{
   fetchData()
@@ -21,11 +40,18 @@ useEffect(()=>{
 
 if(loading){
   return (
-    <div className="App bg-orange-200 w-full h-screen font-sans">
+    <div className="App bg-orange-100 w-full h-screen font-sans">
 <Loading/>
     </div>
   );
 }
+  return(
+  
+    <div>
+      <Tours tourData={tourData}/>
+    </div>
+  )
+
 
 }
 
